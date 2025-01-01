22504a6467908e7d272d5b90a01c4b020dae5d9d
Gabrielle <gabrielle@Gabrielles-MacBook-Pro.local>
2025-01-01 01:33:35 -0600

    update pipeline.py, add arguments

--- week1_docker_gcp_terraform/2_docker_sql/pipeline.py
+++ week1_docker_gcp_terraform/2_docker_sql/pipeline.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import sys
+
+print(sys.argv)
+
+print(sys.argv[0]) #return the file name pipeline.py
+
+print(sys.argv[1]) #return the second argument
 
 print('pandas and pyarrow package installed')
